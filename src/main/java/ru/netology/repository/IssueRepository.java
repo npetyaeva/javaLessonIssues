8e2ebf39d2123d988ed75185e00bffa46da8efa9
Natalia Petyaeva <npetyaeva@gmail.com>
2021-11-27 21:15:16 +0300

    Added IssueRepository and IssueRepositoryTest

---
+++ src/main/java/ru/netology/repository/IssueRepository.java
@@ -0,0 +1,31 @@
+package ru.netology.repository;
+
+import ru.netology.domain.Issue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class IssueRepository {
+    private final Collection<Issue> items = new ArrayList<Issue>();
+
+    public void save(Issue item) {
+        items.add(item);
+    }
+
+    public Collection<Issue> findAll() {
+        return items;
+    }
+
+    public void removeById(int id) {
+        items.removeIf(item -> item.getId() == id);
+    }
+
+    public void saveAll(Collection<Issue> items) {
+        this.items.addAll(items);
+    }
+
+    public void removeAll(Collection<Issue> items) {
+
+        this.items.removeAll(items);
+    }
+}
---
+++ src/test/java/ru/netology/repository/IssueRepositoryTest.java
@@ -0,0 +1,69 @@
+package ru.netology.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Issue;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IssueRepositoryTest {
+    private final IssueRepository repo = new IssueRepository();
+
+    private final Issue first = new Issue(
+            1, "Title1", "Description1", "AuthorOne", true, Set.of("typeOne"),
+            Set.of("labelGreen"), Set.of("themeOne"), Set.of("statusOne")
+    );
+    private final Issue second = new Issue(
+            2, "Title2", "Description2", "AuthorTwo", true, Set.of("typeOne"),
+            Set.of("labelRed"), Set.of("themeTwo"), Set.of("statusTwo")
+    );
+    private final Issue third = new Issue(
+            3, "Title3", "Description3", "AuthorOne", true, Set.of("typeTwo"),
+            Set.of("labelBlue"), Set.of("themeOne"), Set.of("statusThree")
+    );
+    private final Issue fourth = new Issue(
+            4, "Title4", "Description4", "AuthorThree", false, Set.of("typeThree"),
+            Set.of("labelGreen"), Set.of("themeThree"), Set.of("statusOne")
+    );
+    private final Issue fifth = new Issue(
+            5, "Title5", "Description5", "AuthorFourth", true, Set.of("typeOne"),
+            Set.of("labelRed"), Set.of("themeFourth"), Set.of("statusOne")
+    );
+    private final Issue sixth = new Issue(
+            6, "Title6", "Description6", "AuthorTwo", false, Set.of("typeOne"),
+            Set.of("labelOrange"), Set.of("themeFifth"), Set.of("statusTwo")
+    );
+
+    @BeforeEach
+    void setUp() {
+        repo.save(first);
+        repo.save(second);
+        repo.save(third);
+    }
+
+    @Test
+    void shouldRemoveById() {
+        List<Issue> expected = List.of(first, third);
+        repo.removeById(2);
+        assertEquals(expected, repo.findAll());
+    }
+
+    @Test
+    void shouldSaveAll() {
+        List<Issue> expected = List.of(first, second, third, fourth, fifth, sixth);
+        List<Issue> list = List.of(fourth, fifth, sixth);
+        repo.saveAll(list);
+        assertEquals(expected, repo.findAll());
+    }
+
+    @Test
+    void shouldRemoveAll() {
+        List<Issue> expected = List.of(first);
+        List<Issue> list = List.of(second, third);
+        repo.removeAll(list);
+        assertEquals(expected, repo.findAll());
+    }
+}
